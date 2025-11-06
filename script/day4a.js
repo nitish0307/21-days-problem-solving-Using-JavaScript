1d27c4e55b415fed8a793d5b3e4014adde082e76
Nitish <nitishm1803@gmail.com>
2025-11-06 21:46:16 +0530

    day 4a

---
+++ script/day4a.js
@@ -0,0 +1,12 @@
+function splitNumberIntoDigits(n) {
+  const digits = [];
+  while (n > 0) {
+    const digit = n % 10;      // get last digit
+    digits.unshift(digit);     // add to the beginning of the array
+    n = Math.floor(n / 10);    // remove last digit
+  }
+  return digits;
+}
+
+// Example:
+console.log(splitNumberIntoDigits(12345)); // [1, 2, 3, 4, 5]
